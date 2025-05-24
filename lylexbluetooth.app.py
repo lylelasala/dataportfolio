7ee34398f55f9c5d716c95b446088b1a7710e2ce
lylexbluetooth <102710270+lylelasala@users.noreply.github.com>
2025-05-24 13:24:32 -0700

    Update lylexbluetooth.app.py

--- lylexbluetooth.app.py
+++ lylexbluetooth.app.py
@@ -1,34 +1,29 @@
 import streamlit as st
 
-# Apple Music embed code (cleaned up)
-apple_music_embed = """
-<iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0"
-height="150" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;"
-sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-src="https://embed.music.apple.com/us/song/beautiful-girls/1650862741"></iframe>
+# SoundCloud Embed Code with Interactive Player
+soundcloud_embed = """
+<iframe width="100%" height="155" scrolling="no" frameborder="no" allow="autoplay"
+src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1530937807&color=%238f5a4b&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
 """
 
-# Streamlit app layout
+# Streamlit App Layout
 st.title("┬─┬ ☕︎ノ( º , ºノ) internet café")
 st.subheader("*prototype")
 
 # Display "Song of the Day"
-st.write("🔊 **song of the day:** beautiful girls - sally boy")
+st.write("🔊 **** Kendrick Lamar - The Hillbillies (2000s remix)")
 
-# Embed Apple Music player
-st.components.v1.html(apple_music_embed, height=175)
+# Embed SoundCloud Player
+st.components.v1.html(soundcloud_embed, height=150)
 
-# Portfolio projects
+# Portfolio Projects
 st.header("proj.")
-projects = {
-    "american sign language finger spelling recognition": "https://www.strongasl.com/learn/home",
-    "asian american philippine basketball association statbook": "https://www.facebook.com/photo.php?fbid=975154716181166&set=a.105158656514114&type=3",
-    "photography": "http://vsco.co/bluetoothbrownheartdotmp4?share=MTc0NTAzOTc3Mg%3D%3D",
-    "soundtrap": "https://www.soundtrap.com/home/creator/projects/folders/63644883"
+projects = {"soundtrap": "https://www.soundtrap.com/home/creator/projects/folders/63644883"
 }
 
 for project, link in projects.items():
     st.write(f"[{project}]({link})")
 
 # Footer (added working link)
-st.write("🔗 [Visit my full portfolio]()")
+st.write("🔗 [Visit my full portfolio](https://bluetoothbrownheartdotmp4.streamlit.app/)")
+
